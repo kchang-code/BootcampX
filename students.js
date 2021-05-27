4951ea0206af92c8ccec28ce10183d5caf05881b
kchang-code <81448949+kchang-code@users.noreply.github.com>
2021-05-27 10:49:11 +0800

    added parameterized queries

--- students.js
+++ students.js
@@ -10,14 +10,15 @@ const pool = new Pool({
 // console.log(input);
 let input = process.argv[2];
 let num = process.argv[3];
+let value = [process.argv[2], process.argv[3]];
 
 pool.query(`
 SELECT students.id as student_id, cohorts.name AS cohort, students.name AS name
 FROM students
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${input}%'
-LIMIT ${num};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, value)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
